d65b007b4f5f112bdfa2bb7d6a3f942c6f3ad651
Taras Kalapun <t.kalapun@gmail.com>
2014-11-19 17:39:19 +0100

    Fix for iOS 8 DateFormatter issue on 12/24 H

--- ios/AdyenClientsideEncryptionDemo/AdyenClientsideEncryptionDemo/Card.m
+++ ios/AdyenClientsideEncryptionDemo/AdyenClientsideEncryptionDemo/Card.m
@@ -52,6 +52,7 @@
     static NSDateFormatter* instance;
     dispatch_once(&once, ^{
         instance = [[NSDateFormatter alloc] init];
+        instance.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
         instance.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
         instance.timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
         instance.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
